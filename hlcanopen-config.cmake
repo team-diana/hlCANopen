3b23a4a025b65cfe542d4a5aa184ebc49d76d39a
clynamen <clynamen@gmail.com>
2015-04-12 15:03:01 +0200

    fix hlcanopen-config.cmake installation dir

--- hlcanopen-config.cmake
+++ hlcanopen-config.cmake
@@ -1,3 +1,3 @@
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-set(hlcanopen_LIBRARIES ${SELF_DIR})
-get_filename_component(hlcanopen_INCLUDE_DIRS "${SELF_DIR}/../include/hlcanopen" ABSOLUTE)
+set(hlcanopen_LIBRARIES ${SELF_DIR}/../../..)
+get_filename_component(hlcanopen_INCLUDE_DIRS "${SELF_DIR}/../../../../include/hlcanopen" ABSOLUTE)
