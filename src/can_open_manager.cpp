0c65b066e98566fcce134e3cc7094c581eb11288
clynamen <clynamen@gmail.com>
2015-09-30 16:45:14 +0200

    split header and source 2

--- src/can_open_manager.cpp
+++ src/can_open_manager.cpp
@@ -18,21 +18,92 @@
 #include <mutex>
 
 namespace hlcanopen {
-
 class CanOpenManager {
 
 public:
-    CanOpenManager(CanCard& card, std::chrono::milliseconds sleepInterval = std::chrono::milliseconds(150));
+    CanOpenManager(CanCard& card, std::chrono::milliseconds sleepInterval = std::chrono::milliseconds(150))  :
+        card(card),
+        running(false),
+        intervalSleepTime(sleepInterval)
+    {}
+
     virtual ~CanOpenManager() {}
-    void setupLogging();
-    void setupLoggingUsingConfigurationDir(const char* dirname);
-    void setDefaultFutureExecutor(std::shared_ptr<folly::Executor> ex);
-    void run();
-    void resetCommunication(u_int8_t nodeId);
-    void resetNode(u_int8_t nodeId);
-    void startRemoteNode(u_int8_t nodeId);
-    void sendNMT(uint8_t command, uint8_t CANid);
-    void setSdoAccessLocal(NodeId nodeId, SDOIndex sdoIndex, EntryAccess access);
+
+    void setupLogging() {
+        setupLoggingUsingConfigurationDir("logging_conf");
+    }
+
+    void setupLoggingUsingConfigurationDir(const char* dirname) {
+        loadConfigFilesFromDir(dirname);
+    }
+
+    void setDefaultFutureExecutor(std::shared_ptr<folly::Executor> ex) {
+        executor = ex;
+    }
+
+    void run() {
+        running = true;
+
+        auto timeoutWatchdogThread = std::thread([&]() {
+            while(running) {
+                std::this_thread::sleep_for(intervalSleepTime);
+                {
+                    std::unique_lock<std::mutex> lock(mutex);
+                    for (auto const &it : nodeManagers) {
+                        it.second->updateQueue();
+                    }
+                }
+            }
+        });
+
+        while(running) {
+            CanMsg newMsg = card.read();
+            {
+                std::unique_lock<std::mutex> lock(mutex);
+                NodeId nodeId = newMsg.cobId.getNodeId();
+                if(nodeId == 0) {
+                    // broadcast
+                } else if(nodeManagers.find(nodeId) == nodeManagers.end()) {
+                    CLOG(WARNING, "canopen_manager") << "Received msg with unknown node id: " << nodeId;
+                } else {
+                    nodeManagers[nodeId]->newMsg(newMsg);
+                }
+            }
+
+            std::this_thread::sleep_for(intervalSleepTime);
+        }
+
+        timeoutWatchdogThread.join();
+    }
+
+
+    void resetCommunication(u_int8_t nodeId)
+    {
+        sendNMT(NMT_RESET_COMMUNICATION, nodeId);
+    }
+
+    void resetNode(u_int8_t nodeId)
+    {
+        sendNMT(NMT_RESET_NODE, nodeId);
+    }
+
+    void startRemoteNode(u_int8_t nodeId)
+    {
+        sendNMT(NMT_START_REMOTE_NODE, nodeId);
+    }
+
+    void sendNMT(uint8_t command, uint8_t CANid)
+    {
+        CanMsg msg;
+
+        msg.cobId = COBId(0, 0);
+        msg[0] = command;
+        msg[1] = CANid;
+
+        CLOG(INFO, "canopen_manager") << "sending NMT message " << std::hex << (int) command << " to canId: " << std::hex << (int) CANid;
+
+        card.write(msg);
+    }
 
     template<typename T> T readSdoLocal(NodeId nodeId, SDOIndex sdoIndex) {
         assertNodeExist(nodeId);
@@ -44,6 +115,10 @@ public:
         assertNodeExist(nodeId);
         std::unique_lock<std::mutex> lock(mutex);
         return nodeManagers[nodeId]->writeSdoLocal(sdoIndex, value);
+    }
+
+    void setSdoAccessLocal(NodeId nodeId, SDOIndex sdoIndex, EntryAccess access) {
+        nodeManagers[nodeId]->setSdoAccessLocal(sdoIndex, access);
     }
 
     template<typename T> folly::Future<T> readSdoRemote(NodeId nodeId, const SDOIndex& sdoIndex,
@@ -78,12 +153,24 @@ public:
         nodeManagers[nodeId]->writeSdoRemote<T>(sdoIndex, value, callback, timeout);
     }
 
-    void initNode(NodeId nodeId, NodeManagerType type);
-    void stop();
+
+    void initNode(NodeId nodeId, NodeManagerType type) {
+        nodeManagers.emplace(nodeId, std::make_unique<NodeManager>(nodeId, card, type));
+    }
+
+    void stop() {
+        running = false;
+    }
 
 private:
-    void assertNodeExist(NodeId nodeId);
-    void initNodeIfNonExistent(NodeId nodeId, NodeManagerType type);
+    void assertNodeExist(NodeId nodeId) {
+        ASSERT_MSG_COUT(nodeManagers.find(nodeId) == nodeManagers.end(),
+                        "No node with id: " << nodeId << " was created yet");
+    }
+
+    void initNodeIfNonExistent(NodeId nodeId, NodeManagerType type) {
+        nodeManagers.emplace(nodeId, std::make_unique<NodeManager>(nodeId, card, type));
+    }
 
     template<typename T> folly::Future<T> addExecutorIfAny(folly::Future<T>&& fut) {
         if(executor) {
